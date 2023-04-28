6a1b4ca61ea4d5cb0667217ee79fd397d0beb809
Mary Mbugua <mbuguamels@gmail.com>
2023-04-28 10:26:47 +0300

    created the Dividend class

--- src/main/java/com/sacco/saccoapp/SaccoAppApplication.java
+++ src/main/java/com/sacco/saccoapp/SaccoAppApplication.java
@@ -3,8 +3,11 @@ package com.sacco.saccoapp;
 import com.sacco.saccoapp.loan.Loan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -15,7 +18,15 @@ public class SaccoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaccoAppApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 
 
 }
