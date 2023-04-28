b71996898e30c0b44bfef9a594f3060ec8233fce
Mary Mbugua <mbuguamels@gmail.com>
2023-04-28 10:33:58 +0300

    created the Dvidend controller

---
+++ src/main/java/com/sacco/saccoapp/dividend/DividendController.java
@@ -0,0 +1,12 @@
+package com.sacco.saccoapp.dividend;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/dividend")
+@AllArgsConstructor
+public class DividendController {
+
+}
